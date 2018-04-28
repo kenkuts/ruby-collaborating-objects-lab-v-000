ec17ff0a548f37177b347306a16f6ce5a0dffb16
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-28 04:38:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,6 +10,14 @@ class Artist
     save
   end
 
+  def self.find_or_create_by_name(name)
+    @@all.detect { |i| i.name == name } || self.new(name)
+  end
+
+  def print_songs
+    @songs.each { |song| puts song.name }
+  end
+
   def add_song(song)
     @songs << song
   end
@@ -22,11 +30,4 @@ class Artist
     @@all
   end
 
-  def self.find_or_create_by_name(name)
-    @@all.detect { |i| i.name == name } || self.new(name)
-  end
-
-  def print_songs
-    @songs.each { |song| puts song.name }
-  end
 end
